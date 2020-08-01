0a2b74b0c137a8337be14d8f82df11e4f693f73c
Jhonatan Lavado <jhonatanlavadovalentin@gmail.com>
2020-08-01 03:38:14 -0500

    Algunos cambios realizados

--- Prueba.Entidades/Program.cs
+++ Prueba.Entidades/Program.cs
@@ -16,7 +16,7 @@ namespace Prueba.Entidades
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run();
         }
     }
 }
